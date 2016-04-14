118b8d1e474dcea792084e8200f5318a8431977c
christroutner <chris.troutner@gmail.com>
2016-04-14 12:50:53 -0700

    fixing functionality with empty DB.

---
+++ keystone-files/keystone.js
@@ -0,0 +1,69 @@
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').load();
+
+// Require keystone
+var keystone = require('keystone');
+var handlebars = require('express-handlebars');
+
+// Initialise Keystone with your project's configuration.
+// See http://keystonejs.com/guide/config for available options
+// and documentation.
+
+keystone.init({
+
+        'name': 'connextcms-temp',
+        'brand': 'ConnextCMS',
+
+        'less': 'public',
+        'static': 'public',
+        'favicon': 'public/favicon.ico',
+        'views': 'templates/views',
+        'view engine': 'hbs',
+
+        'custom engine': handlebars.create({
+                layoutsDir: 'templates/views/layouts',
+                partialsDir: 'templates/views/partials',
+                defaultLayout: 'default',
+                helpers: new require('./templates/views/helpers')(),
+                extname: '.hbs'
+        }).engine,
+
+        'auto update': true,
+        'session': true,
+        'auth': true,
+        'user model': 'User'
+
+});
+
+// Load your project's Models
+
+keystone.import('models');
+
+// Setup common locals for your templates. The following are required for the
+// bundled templates and layouts. Any runtime locals (that should be set uniquely
+// for each request) should be added to ./routes/middleware.js
+
+keystone.set('locals', {
+        _: require('underscore'),
+        env: keystone.get('env'),
+        utils: keystone.utils,
+        editable: keystone.content.editable
+});
+
+// Load your project's Routes
+
+keystone.set('routes', require('./routes'));
+
+// Configure the navigation bar in Keystone's Admin UI
+
+keystone.set('nav', {
+        'posts': ['posts', 'post-categories'],
+        'galleries': 'galleries',
+        'enquiries': 'enquiries',
+        'users': 'users'
+});
+
+// Start Keystone to connect to your database and initialise the web server
+
+keystone.start();
