2ccc03146ef16d40cdbb4f40c9748448b9180ea2
christroutner <chris.troutner@gmail.com>
2016-04-15 11:17:26 -0700

    fixing functionality with empty DB

--- public/js/app/model/PageSectionCollection.js
+++ public/js/app/model/PageSectionCollection.js
@@ -16,7 +16,6 @@ define([
     //Parse allows me to massage non-standard data before it is returned to the collection.
     parse: function(response) {
       //debugger;
-      return response.pagesection;
       
       if(response.pagesection.length == 0) {
         log.push('Empty data returned by server when trying to retrieve PageSection collection. Most likely due to a new DB.');
