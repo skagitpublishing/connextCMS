ccbaff6c99b0f8f51c52014a3acba0a8e4418d12
christroutner <chris.troutner@gmail.com>
2016-04-16 07:45:49 -0700

    fixing functionality when using an empty DB.

--- public/js/app/views/pagesAddNewView.js
+++ public/js/app/views/pagesAddNewView.js
@@ -250,6 +250,7 @@ define([
 
           //Don't try to create a new page without a title.
           if( this.$el.find('#pageTitle').val() == "" ) {
+            debugger;
             alert('Please give the page a title.');
             global.pagesAddNewView.$el.find('#successWaitingModal').modal('hide');
             return;
