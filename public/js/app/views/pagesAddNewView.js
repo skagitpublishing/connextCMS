5c8c4c3c077936b175ad27b62628852938f9f826
christroutner <chris.troutner@gmail.com>
2016-04-17 16:11:18 -0700

    trying to get date picker to autoclose upon date selection.

--- public/js/app/views/pagesAddNewView.js
+++ public/js/app/views/pagesAddNewView.js
@@ -112,13 +112,13 @@ define([
         }
         
         //Initialize datepicker
-        /*
+        
         debugger;
-        this.$el.find('#publishedDate').datepicker({
-          autoclose: true,
-          todayHighlight: true
-        });
-        */
+        //this.$el.find('#publishedDate').datepicker({
+        //  autoclose: true,
+        //  todayHighlight: true
+        //});
+        
         
       } catch(err) {
         console.error('Error while trying to render pagesAddNewView. Error message: ');
