2306e6028ad80348b47a52b5b37baf2a3d2ccf26
christroutner <chris.troutner@gmail.com>
2016-04-18 14:11:28 -0700

    fixing bug around switching between posts and pages.

--- public/js/app/views/pagesAddNewView.js
+++ public/js/app/views/pagesAddNewView.js
@@ -80,7 +80,7 @@ define([
             //The setup function runs when the TinyMCE editor has finished loading. It's kind of like the document.ready() function.
             setup: function (ed) {
               ed.on('init', function(args) {
-                //debugger;  
+                debugger;  
 
                 global.tinymce.initialized = true;
                 global.tinymce.currentView = 'pages';
