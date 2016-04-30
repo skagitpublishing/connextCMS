c827aa0453c717288c320adacd4993ac1745c9d8
christroutner <chris.troutner@gmail.com>
2016-04-30 08:31:12 -0700

    Fixing bug created when I deleted frontendwidgets

--- routes/views/dashboard.js
+++ routes/views/dashboard.js
@@ -5,14 +5,14 @@ exports = module.exports = function(req, res) {
         var view = new keystone.View(req, res);
         var locals = res.locals;
 
-	locals.user = req.user;
-	locals.user.password = "";
+        locals.user = req.user;
+        locals.user.password = "";
 
         // Set locals
         locals.section = 'dashboard';
 
         // Load the galleries by sortOrder
-        view.query('frontendcollections', keystone.list('FrontendWidget').model.find().sort('sortOrder'));
+        //view.query('frontendcollections', keystone.list('FrontendWidget').model.find().sort('sortOrder'));
 
         // Render the view
         view.render('dashboard');
