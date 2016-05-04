3beb48413d6a2fedcd0025bb24325c82f61c4083
christroutner <chris.troutner@gmail.com>
2016-05-04 12:12:54 -0700

    Working on issue #13, white stripe bug with Bootstrap CSS

--- public/js/lib/bootstrap.3.3.6.js
+++ public/js/lib/bootstrap.3.3.6.js
@@ -1183,12 +1183,14 @@ if (typeof jQuery === 'undefined') {
   }
 
   Modal.prototype.setScrollbar = function () {
+    debugger;
     var bodyPad = parseInt((this.$body.css('padding-right') || 0), 10)
     this.originalBodyPad = document.body.style.paddingRight || ''
     if (this.bodyIsOverflowing) this.$body.css('padding-right', bodyPad + this.scrollbarWidth)
   }
 
   Modal.prototype.resetScrollbar = function () {
+    debugger;
     this.$body.css('padding-right', this.originalBodyPad)
   }
 
