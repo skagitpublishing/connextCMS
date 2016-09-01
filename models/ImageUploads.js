5c59f0dca0b698f17e108d760666a305cc88279c
Chris Troutner <chris.troutner@gmail.com>
2016-09-01 12:33:00 -0700

    updating keystone models with new file type.

--- models/ImageUploads.js
+++ models/ImageUploads.js
@@ -7,14 +7,14 @@ var Types = keystone.Field.Types;
  * A database model for uploading images to the local file system
  */
 
-/*
+
 var ImageUpload = new keystone.List('ImageUpload');
 
-var storage = new keystone.Storage({
+var myStorage = new keystone.Storage({
     adapter: keystone.Storage.Adapters.FS,
     fs: {
-        path: keystone.expandPath('./uploads'), // required; path where the files should be stored
-        publicPath: '/public/uploads', // path where files will be served
+        path: keystone.expandPath('./uploads/images'), // required; path where the files should be stored
+        publicPath: '/public/uploads/images', // path where files will be served
     }
 });
 
@@ -24,12 +24,13 @@ ImageUpload.add({
   image: { 
 		//type: Types.LocalFile, 
     type: Types.File,
-		dest: 'public/uploads/images', 
-		label: 'Image',
-		allowedTypes: [ 'image/jpeg', 'image/png', 'image/gif'],
-		filename: function(item, file) {
-			return item.id + '.' + file.extension;
-		}
+    storage: myStorage
+		//dest: 'public/uploads/images', 
+		//label: 'Image',
+		//allowedTypes: [ 'image/jpeg', 'image/png', 'image/gif'],
+		//filename: function(item, file) {
+		//	return item.id + '.' + file.extension;
+		//}
 	},
 	alt1: { type: String },
         attributes1: { type: String },
@@ -46,4 +47,4 @@ ImageUpload.add({
 
 ImageUpload.defaultColumns = 'name';
 ImageUpload.register();
-*/
+
