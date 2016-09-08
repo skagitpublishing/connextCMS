025bdfba471335d8e32b190660276a9d1606debc
Chris Troutner <chris.troutner@gmail.com>
2016-09-07 18:43:58 -0700

    adding forecolor and backcolor to TinyMCE toolbar on pages and posts add new view

--- public/js/app/views/postsAddNewView.js
+++ public/js/app/views/postsAddNewView.js
@@ -59,7 +59,7 @@ define([
             selector: '#postContent',
             //menubar: 'edit view format insert',
             menubar: false,
-            toolbar: 'bold, italic, underline, strikethrough, alignleft, aligncenter, alignright, alignjustify, bullist, numlist, outdent, indent, removeformat, subscript, superscript, link, formatselect, fontselect, image_gallery, file_link, code',
+            toolbar: 'bold, italic, underline, strikethrough, alignleft, aligncenter, alignright, alignjustify, bullist, numlist, outdent, indent, removeformat, subscript, superscript, link, formatselect, fontselect, forecolor, backcolor, image_gallery, file_link, code',
             plugins: 'code, image_gallery, link, file_link',
             extended_valid_elements: "a[class|name|href|target|title|onclick|rel|id|download],button[class|name|href|target|title|onclick|rel|id],script[type|src],iframe[src|style|width|height|scrolling|marginwidth|marginheight|frameborder|allowfullscreen],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],$elements",
             relative_urls: false,
