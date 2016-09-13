ee08b67d9a6f2a8ba95b32feabf9548071a513eb
Chris Troutner <chris.troutner@gmail.com>
2016-09-13 14:19:54 -0700

    Integrating new modal view with Categories view

--- public/js/app/views/modalView.js
+++ public/js/app/views/modalView.js
@@ -1,5 +1,10 @@
-/*global define*/
-//Define libraries this file depends on.
+/*
+The modal view is a single modal that lives in a <div> on the dashboard.hbs page, with the ID 'modalView'. 
+All other views use this single modal to display information rather than implement individual modal windows
+per Backbone View, which what I tried to do originally. That led to problems, so this is the fix.
+*/
+
+
 define([
 	'jQuery-2.1.4.min',
 	'underscore_1.3.3',
@@ -25,24 +30,41 @@ define([
 
 		},
 
+    modalData: {
+      title: 'Modal Title',
+      body: '<p>Modal body text.</p>',
+      btn1: '<button type="button" class="btn btn-default" id="mainModalBtn1" data-dismiss="modal">Close</button>',
+      btn2: '<button type="button" class="btn btn-primary" id="mainModalBtn2" >Save changes</button>'
+    },
+    
     render: function () {
-      //debugger;
+      debugger;
       
       this.$el.html(this.template);
       
+      //Show the div containing the modal.
       $('#modalView').show();
       
-      //this.$el.find('#mainModal').modal('show');
+      //Update the modal with the default data.
+      this.$el.find('#mainModalTitle').text(this.modalData.title);
+      this.$el.find('#mainModalBody').html(this.modalData.body);
+      var footer = this.$el.find('#mainModalFooter');
+      var btn1 = this.$el.find('#mainModalBtn1');
       
 			return this;
 		},
     
+    
+    
     openModal: function() {
       this.$el.find('#mainModal').modal('show');
     },
+    
     closeModal: function() {
       this.$el.find('#mainModal').modal('hide');
     }
+    
+    
     
 	});
 
