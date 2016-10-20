06db18d8025eaae1a78f0640a7f2fa06257a67c0
Chris Troutner <chris.troutner@gmail.com>
2016-10-20 09:14:46 -0700

    Debugging and testing ideas.

---
+++ keystone-files/update.js
@@ -0,0 +1,23 @@
+module.exports = function (req, res) {
+        var keystone = req.keystone;
+        debugger;
+        if (!keystone.security.csrf.validate(req)) {
+                return res.apiError(403, 'invalid csrf');
+        }
+        req.list.model.findById(req.params.id, function (err, item) {
+                if (err) return res.status(500).json({ error: 'database error', detail: err });
+                if (!item) return res.status(404).json({ error: 'not found', id: req.params.id });
+                req.list.updateItem(item, req.body, { files: req.files, user: req.user }, function (err) {
+                        if (err) {
+                                var status = err.error === 'validation errors' ? 400 : 500;
+                                var error = err.error === 'database error' ? err.detail : err;
+                                return res.apiError(status, error);
+                        }
+                        // Reload the item from the database to prevent save hooks or other
+                        // application specific logic from messing with the values in the item
+                        req.list.model.findById(req.params.id, function (err, updatedItem) {
+                                res.json(req.list.getData(updatedItem));
+                        });
+                });
+        });
+};
