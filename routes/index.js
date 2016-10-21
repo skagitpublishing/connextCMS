2bf954a6960f5301149e1b5794415609f965b0cc
Chris Troutner <chris.troutner@gmail.com>
2016-10-21 08:14:36 -0700

    Adding /changepassword path to test if CSRF token is served on this page

--- routes/index.js
+++ routes/index.js
@@ -103,6 +103,6 @@ exports = module.exports = function(app) {
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
   app.get('/dashboard', middleware.requireUser, routes.views.dashboard);
-  appget('/changepassword', middleware.requireUser, routes.views.changepassword);
+  app.get('/changepassword', middleware.requireUser, routes.views.changepassword);
 	
 };
