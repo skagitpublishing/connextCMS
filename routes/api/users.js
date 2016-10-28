bf88f97523f85604a70343203e9017001caadf2c
Chris Troutner <chris.troutner@gmail.com>
2016-10-27 17:44:42 -0700

    moving list of superusers to an external file.

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,7 +5,7 @@ var User = keystone.list('User');
 var security = keystone.security;
 
 //var superusers = keystone.get('superusers');
-var Superusers = require('./superusers.js');
+var Superusers = require('./models/superusers.js');
 var superusers = new Superusers.Constructor();
 
 debugger;
