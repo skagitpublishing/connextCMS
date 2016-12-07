0c124d1240b26ef3f4396786022469b7a1813bb7
Chris Troutner <chris.troutner@gmail.com>
2016-12-07 11:44:03 -0800

    Developing front end widget view

--- public/js/app/views/frontEndWidgetView.js
+++ public/js/app/views/frontEndWidgetView.js
@@ -122,6 +122,7 @@ debugger;
         var colIndex = 0;
         var tmpRow = this.$el.find('#widgetImages').find('.scaffold').clone();
         tmpRow.removeClass('scaffold');
+        tmpRow.find('.label').remove(); //Remove the label.
         
         //Loop through all the images
         for(var i=0; i < imgArray.length; i++) {
