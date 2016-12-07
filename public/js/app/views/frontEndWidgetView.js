79519f9cece545c63d88a1ae7397253f40fcdb68
Chris Troutner <chris.troutner@gmail.com>
2016-12-07 11:21:14 -0800

    Developing front end widget view

--- public/js/app/views/frontEndWidgetView.js
+++ public/js/app/views/frontEndWidgetView.js
@@ -88,6 +88,42 @@ define([
       
       this.$el.find('#widgetTitle').val(item.get('title'));
       this.$el.find('#widgetDesc').val(item.get('desc'));
+
+      var htmlArray = item.get('htmlArray');
+      var imgArray = item.get('imgUrl');
+debugger;      
+      //BEGIN POPULATION OF HTML ARRAY
+      if((htmlArray.length == 0) || (htmlArray.length == undefined)) {
+        //Do nothing. Leave the default HTML the way it is.
+      } else {
+        
+        for(var i=0; i < htmlArray.length; i++) {
+          var tmpEntry = this.$el.find('#widgetHTML').find('.scaffold').clone(); //Clone the scaffolding
+        
+          tempEntry.removeClass('scaffold'); //Remove the scaffold class
+          tmpEntry.find('button').click(this.deleteHtml(i)); //Assign a click handler to the delete button
+          tmpEntry.find('textarea').val(htmlArray[i]); //Populate the text box
+          this.$el.find('#widgetHTML').append(tmpEntry);
+        }
+        
+        //Remove the scaffolding element
+        this.$el.find('#widgetHTML').find('.scaffold').remove();
+      }
+      //END POPULATION OF HTML ARRAY
+      
+      //BEGIN POPULATION OF IMAGE ARRAY
+      if((imgArray.length == 0) || (imgArray.length == undefined)) {
+        //Do nothing. Leave the default image layout the way it is.
+      } else {
+        
+      }
+      //END POPULATION OF IMAGE ARRAY
+    },
+    
+    //This function is called when the user clicks on the delete button assigned to an HTML array 
+    //element. It's purpose is to remove the array entry from the model.
+    deleteHtml: function(index) {
+      debugger;
     }
     
 
