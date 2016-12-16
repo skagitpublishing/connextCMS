7755bc0f76ca7b7ed68a57a5ae8646a0b2225cfe
Chris Troutner <chris.troutner@gmail.com>
2016-12-16 14:29:35 -0800

    Developing the combination of TinyMCE and the Front End Widget View.

--- public/js/app/model/FrontEndWidgetCollection.js
+++ public/js/app/model/FrontEndWidgetCollection.js
@@ -59,7 +59,7 @@ define([
           //debugger;
           this.refreshWidget = false;
           
-          global.editWidgetView.render(global.editWidgetView.targetWidget);
+          global.editWidgetView.render(global.editWidgetView.targetWidget, global.tinymce.currentModelIndex);
         }
 
         //Assumption: this funciton is only called when opening the image gallery. Therefore we need to call it again and
