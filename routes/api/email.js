bbb117e3c0f8c39a145c18de7c53effd0c2b51d2
Chris Troutner <chris.troutner@gmail.com>
2017-01-19 11:29:56 -0800

    Adding email API

--- routes/api/email.js
+++ routes/api/email.js
@@ -3,7 +3,7 @@ var keystone = require('keystone');
 var Mailgun = require('mailgun-js'); //Mailgun API library.
 
 //Read in the server settings file.
-var serverSettings = require('../../../public/js/serversettings.js');
+var serverSettings = require('../../public/js/serversettings.js');
 var serverData = serverSettings.getServerData();
 
 /**
