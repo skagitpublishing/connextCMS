87c00a0c5b578190e4d021386f90c40028e56385
Chris Troutner <chris.troutner@gmail.com>
2017-01-19 12:06:14 -0800

    Developing email API and sendlog to use it.

--- public/js/lib/logs.js
+++ public/js/lib/logs.js
@@ -15,6 +15,24 @@ function sendLog() {
   obj.log = log;
   $.get('/api/email/sendlog', obj, function(data) {
     console.log('Log file emailed to administrator.');
+  })
+  .fail(function( jqxhr, textStatus, error ) {
+    debugger;
+
+    var err = textStatus + ", " + error;
+
+    try {
+      if(jqxhr.responseJSON.detail == "invalid csrf") {
+        global.modalView.errorModal('Update failed due to a bad CSRF token. Please log out and back in to refresh your CSRF token.');
+        return;
+      } else {
+        global.modalView.errorModal("Request failed because of: "+error+'. Error Message: '+jqxhr.responseText);
+        console.log( "Request Failed: " + error );
+        console.error('Error message: '+jqxhr.responseText);
+      }
+    } catch(err) {
+      console.error('Error trying to retrieve JSON data from server response.');
+    }            
   });
 }
 
