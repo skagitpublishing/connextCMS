eeab0703e7a1af6243b97012d2738194f769bb2c
Chris Troutner <chris.troutner@gmail.com>
2017-01-25 13:17:00 -0800

    Developing the Site Settings View.

--- routes/api/serversettings.js
+++ routes/api/serversettings.js
@@ -28,9 +28,23 @@ exports.getprivate = function(req, res) {
   //  return res.apiError(403, 'Not allowed to access this API. Not ConnextCMS Admin')
   //}
   
-  res.apiResponse({
-    privateSettings: privateSettings
-  })
+  fs.readFile('../../private/privatesettings.json', 'utf8', function(err, data) {
+    if(err) {
+      console.log('Error in /api/serversettings/getprivate while trying to read privatesettings.json file.');
+      console.log(err);
+      //response.send(false); //Send failure
+      res.apiError('file error', err);
+      
+    } else {
+      privateSettings = JSON.parse(data); 
+      
+      res.apiResponse({
+        privateSettings: privateSettings
+      });
+    }
+  });
+  
+  
 }
 
 exports.saveprivate = function(req, res) {
