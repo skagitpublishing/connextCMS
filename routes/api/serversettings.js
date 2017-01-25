7d56f865c0054bedf0ecf7d206b5f5f1fb69fb96
Chris Troutner <chris.troutner@gmail.com>
2017-01-25 13:25:32 -0800

    Developing the Site Settings View.

--- routes/api/serversettings.js
+++ routes/api/serversettings.js
@@ -1,5 +1,5 @@
 var async = require('async'),
-	keystone = require('keystone');
+        keystone = require('keystone');
 
 //var exec = require('child_process').exec;
 var fs = require('fs');
@@ -13,13 +13,13 @@ var publicSettings = require('../../public/js/publicsettings.json');
 
 
 exports.getprivate = function(req, res) {
-  
+
   //Ensure the user making the request is a Keystone Admin
   //var isAdmin = req.user.get('isAdmin');
   //if(!isAdmin) {
   //  return res.apiError(403, 'Not allowed to access this API. Not Keystone Admin.');
   //}
-  
+
   //Since it's possible to spoof the Keystone Admin setting in the current version of the User model,
   //This is a check to make sure the user is a ConnexstCMS Admin
   //var admins = keystone.get('admins');
@@ -27,9 +27,9 @@ exports.getprivate = function(req, res) {
   //if(admins.indexOf(userId) == -1) {
   //  return res.apiError(403, 'Not allowed to access this API. Not ConnextCMS Admin')
   //}
-  
+
   fs.readFile('../../private/privatesettings.json', 'utf8', function(err, data) {
-    /*
+    
     if(err) {
       console.log('Error in /api/serversettings/getprivate while trying to read privatesettings.json file.');
       console.log(err);
@@ -43,10 +43,8 @@ exports.getprivate = function(req, res) {
         privateSettings: privateSettings
       });
     }
-    */
+    
   });
-  
-  
 }
 
 exports.saveprivate = function(req, res) {
@@ -55,7 +53,7 @@ exports.saveprivate = function(req, res) {
   //if(!isAdmin) {
   //  return res.apiError(403, 'Not allowed to access this API. Not Keystone Admin.');
   //}
-  
+
   //Since it's possible to spoof the Keystone Admin setting in the current version of the User model,
   //This is a check to make sure the user is a ConnexstCMS Admin
   //var admins = keystone.get('admins');
@@ -63,9 +61,9 @@ exports.saveprivate = function(req, res) {
   //if(admins.indexOf(userId) == -1) {
   //  return res.apiError(403, 'Not allowed to access this API. Not ConnextCMS Admin')
   //}
-  
+
   var data = req.query;
-  
+
   //Write out the server_settings.json file.
   fs.writeFile('../../private/privatesettings.json', JSON.stringify(data, null, 4), function (err) {
     if(err) {
@@ -73,7 +71,7 @@ exports.saveprivate = function(req, res) {
       console.log(err);
       //response.send(false); //Send failure
       res.apiError('file error', err);
-      
+
     } else {
       console.log('/api/serversettings/save executed. privatesettings.json updated.');
       //response.send(true); //Send acknowledgement that setting were saved successfully.
@@ -92,7 +90,7 @@ exports.savepublic = function(req, res) {
   //if(!isAdmin) {
   //  return res.apiError(403, 'Not allowed to access this API. Not Keystone Admin.');
   //}
-  
+
   //Since it's possible to spoof the Keystone Admin setting in the current version of the User model,
   //This is a check to make sure the user is a ConnexstCMS Admin
   //var admins = keystone.get('admins');
@@ -100,9 +98,9 @@ exports.savepublic = function(req, res) {
   //if(admins.indexOf(userId) == -1) {
   //  return res.apiError(403, 'Not allowed to access this API. Not ConnextCMS Admin')
   //}
-  
+
   var data = req.query;
-  
+
   //Write out the server_settings.json file.
   fs.writeFile('../../public/js/publicsettings.json', JSON.stringify(data, null, 4), function (err) {
     if(err) {
@@ -110,7 +108,7 @@ exports.savepublic = function(req, res) {
       console.log(err);
       //response.send(false); //Send failure
       res.apiError('file error', err);
-      
+
     } else {
       console.log('/api/serversettings/savepublic executed. publicsettings.json updated.');
       //response.send(true); //Send acknowledgement that setting were saved successfully.
