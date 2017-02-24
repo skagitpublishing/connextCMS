b3f0778322be79d55483ef5e127ec47ba03d90a6
Chris Troutner <chris.troutner@gmail.com>
2017-02-23 18:14:11 -0800

    Removing CSRF check from front end widgets.

--- routes/api/frontendwidget.js
+++ routes/api/frontendwidget.js
@@ -116,9 +116,9 @@ exports.update = function(req, res) {
 exports.remove = function(req, res) {
   
   //Ensure the user has a valid CSRF token
-	if (!security.csrf.validate(req)) {
-		return res.apiError(403, 'invalid csrf');
-	}
+	//if (!security.csrf.validate(req)) {
+  //  return res.apiError(403, 'invalid csrf');
+	//}
   
   //Ensure the user making the request is a Keystone Admin
   var isAdmin = req.user.get('isAdmin');
