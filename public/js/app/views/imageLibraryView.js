3258921ef299bc34729131cb83ccfd654e9e4adc
Chris Troutner <chris.troutner@gmail.com>
2017-06-14 10:52:58 -0700

    removing unneeded console log form image library view

--- public/js/app/views/imageLibraryView.js
+++ public/js/app/views/imageLibraryView.js
@@ -374,7 +374,7 @@ define([
       
       var wrapperHeight = $('.content-wrapper').height();
       
-      console.log($('.wrapper').height());
+      //console.log($('.wrapper').height());
       $('#control-sidebar-home-tab').css('overflow-y', 'scroll');
       $('#control-sidebar-home-tab').load('/documentation/core/imagefilelibrary.html', function() {
         $('#control-sidebar-home-tab').height(wrapperHeight);
