7829048ed4363395eba398d538be4962ca2e0cd3
Chris Troutner <chris.troutner@gmail.com>
2017-08-13 09:02:10 -0700

    Converting imageAddNewView to 300px thumnail on displayed canvas after resizing image

--- public/js/app/views/imageAddNewView.js
+++ public/js/app/views/imageAddNewView.js
@@ -259,7 +259,12 @@ define([
       //Append the original image as a canvas
       $('#imageResizeDiv').append(imgOrig);
       Caman('#imageToUploadOriginal', function () {
-              this.render();
+        //Resize to a thumbnail.
+        this.resize({
+            width: 300
+        });
+
+        this.render();
       });
 
     },
